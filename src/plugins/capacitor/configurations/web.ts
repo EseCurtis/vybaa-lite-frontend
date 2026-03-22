d694c6609d14956f518ebfcd07cb40466fbc468c
EseCurtis <curtisese52@gmail.com>
2026-03-22 16:24:19 +0100

    update index.html and web.ts

--- src/plugins/capacitor/configurations/web.ts
+++ src/plugins/capacitor/configurations/web.ts
@@ -1,6 +1,19 @@
+
+import { SafeArea } from "capacitor-plugin-safe-area";
+
 const webConfig = () => {
     document.documentElement.style.setProperty('--statusbar-clearfix', '0px');
     document.documentElement.style.setProperty('--statusbar-clearfix', '25px');
+
+    SafeArea.getSafeAreaInsets().then(({ insets }) => {
+        // alert(JSON.stringify(insets));
+        for (const [key, value] of Object.entries(insets)) {
+            document.documentElement.style.setProperty(
+                `--safe-area-inset-${key}`,
+                `${value+10}px`,
+            );
+        }
+    })
 }
 
 export default webConfig;
