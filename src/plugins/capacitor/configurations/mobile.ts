8ff117f5e16eaa41be7d9a60def59da9f1c3c3a7
EseCurtis <curtisese52@gmail.com>
2026-08-28 13:48:19 +0100

    feat: request microphone permission on startup

--- src/plugins/capacitor/configurations/mobile.ts
+++ src/plugins/capacitor/configurations/mobile.ts
@@ -1,35 +1,34 @@
+import { IS_ANDROID } from '@/shared/constants.shared'
+import { SafeArea } from 'capacitor-plugin-safe-area'
 
-import { IS_ANDROID } from "@/shared/constants.shared";
-import { SafeArea } from "capacitor-plugin-safe-area";
-import { addPushNotificationListeners, createPushNotificationChannels, registerPushNotifications } from "../plugins/push-notification.plugin";
+import { ensureVoiceRecordingPermission } from '../plugins/voice-recorder.plugin'
+import {
+  addPushNotificationListeners,
+  createPushNotificationChannels,
+  registerPushNotifications,
+} from '../plugins/push-notification.plugin'
 
-const mobileConfig = async () => {
-    //return 
-    await Promise.all([
-        await registerPushNotifications().then(async () => {
-        }).catch(() => { }),
-        await Promise.all([
-            await addPushNotificationListeners(),
-            await createPushNotificationChannels()
-        ]).then(() => { }),
-        SafeArea.getSafeAreaInsets().then(({ insets }) => {
-            // alert(JSON.stringify(insets));
-            for (let [key, value] of Object.entries(insets)) {
+const mobileConfig = async (): Promise<void> => {
+  await Promise.allSettled([
+    registerPushNotifications(),
+    addPushNotificationListeners(),
+    createPushNotificationChannels(),
+    ensureVoiceRecordingPermission(),
+    SafeArea.getSafeAreaInsets().then(({ insets }) => {
+      for (const [key, rawValue] of Object.entries(insets)) {
+        let value = rawValue
 
-                if (key == "bottom" && IS_ANDROID) {
-                    // Android has a soft navigation bar that overlaps the app content, so we set a larger bottom inset to account for it.
-                    value += 20
-                }
-                document.documentElement.style.setProperty(
-                    `--safe-area-inset-${key}`,
-                    `${value}px`,
-                );
-            }
-        })
+        if (key === 'bottom' && IS_ANDROID) {
+          value += 20
+        }
 
-    ])
-
-
+        document.documentElement.style.setProperty(
+          `--safe-area-inset-${key}`,
+          `${value}px`,
+        )
+      }
+    }),
+  ])
 }
 
-export default mobileConfig;
+export default mobileConfig
